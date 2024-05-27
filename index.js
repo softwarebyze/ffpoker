be1513f29d51551fed19027c9ef21fd5d6125480
Zachary Ebenfeld <zackebenfeld@gmail.com>
2024-05-27 19:00:23 -0400

    formatted

--- index.js
+++ index.js
@@ -1,18 +1,51 @@
 // Define initial game variables
-const positions = ['QB', 'RB', 'WR', 'Def', 'TE', 'K'];
-const teams = ['Tennessee Titans', 'Kansas City Chiefs', 'San Francisco 49ers', 'Dallas Cowboys'];
+const positions = ["QB", "RB", "WR", "Def", "TE", "K"];
+const teams = [
+  "Tennessee Titans",
+  "Kansas City Chiefs",
+  "San Francisco 49ers",
+  "Dallas Cowboys",
+];
 const teamScores = {
-  'Tennessee Titans': { 'RB': 25, 'QB': 12, 'WR': 10, 'TE': 15, 'Def': 3, 'K': 12 },
-  'Kansas City Chiefs': { 'RB': 16, 'QB': 30, 'WR': 8, 'TE': 20, 'Def': 2, 'K': 1 },
-  'San Francisco 49ers': { 'RB': 30, 'QB': 18, 'WR': 20, 'TE': 16, 'Def': 20, 'K': 2 },
-  'Dallas Cowboys': { 'RB': 10, 'QB': 20, 'WR': 23, 'TE': 12, 'Def': 14, 'K': 8 }
+  "Tennessee Titans": { RB: 25, QB: 12, WR: 10, TE: 15, Def: 3, K: 12 },
+  "Kansas City Chiefs": { RB: 16, QB: 30, WR: 8, TE: 20, Def: 2, K: 1 },
+  "San Francisco 49ers": { RB: 30, QB: 18, WR: 20, TE: 16, Def: 20, K: 2 },
+  "Dallas Cowboys": { RB: 10, QB: 20, WR: 23, TE: 12, Def: 14, K: 8 },
 };
 
 const activePlayersData = {
-  'Tennessee Titans': { 'RB': 'Derrick Henry', 'QB': 'Will Levis', 'WR': 'DeAndre Hopkins', 'TE': 'Chigoziem Okonkwo', 'Def': 'Titans Defense', 'K': 'Nick Folk' },
-  'Kansas City Chiefs': { 'RB': 'Isaiah Pacheco', 'QB': 'Patrick Mahomes', 'WR': 'Skyy Moore', 'TE': 'Travis Kelce', 'Def': 'Chiefs Defense', 'K': 'Harrison Butker' },
-  'San Francisco 49ers': { 'RB': 'Christian McCaffrey', 'QB': 'Brock Purdy', 'WR': 'Deebo Samuel', 'TE': 'George Kittle', 'Def': '49ers Defense', 'K': 'Jake Moody' },
-  'Dallas Cowboys': { 'RB': 'Ezekiel Elliott', 'QB': 'Dak Prescott', 'WR': 'CeeDee Lamb', 'TE': 'Jake Ferguson', 'Def': 'Cowboys Defense', 'K': 'Brandon Aubrey' }
+  "Tennessee Titans": {
+    RB: "Derrick Henry",
+    QB: "Will Levis",
+    WR: "DeAndre Hopkins",
+    TE: "Chigoziem Okonkwo",
+    Def: "Titans Defense",
+    K: "Nick Folk",
+  },
+  "Kansas City Chiefs": {
+    RB: "Isaiah Pacheco",
+    QB: "Patrick Mahomes",
+    WR: "Skyy Moore",
+    TE: "Travis Kelce",
+    Def: "Chiefs Defense",
+    K: "Harrison Butker",
+  },
+  "San Francisco 49ers": {
+    RB: "Christian McCaffrey",
+    QB: "Brock Purdy",
+    WR: "Deebo Samuel",
+    TE: "George Kittle",
+    Def: "49ers Defense",
+    K: "Jake Moody",
+  },
+  "Dallas Cowboys": {
+    RB: "Ezekiel Elliott",
+    QB: "Dak Prescott",
+    WR: "CeeDee Lamb",
+    TE: "Jake Ferguson",
+    Def: "Cowboys Defense",
+    K: "Brandon Aubrey",
+  },
 };
 
 const numPlayers = 4;
@@ -41,42 +74,52 @@ function startGame() {
   activePlayers = [...Array(numPlayers).keys()];
   bettingPhase = 1;
 
-  document.getElementById('teams-drawn').innerHTML = '';
-  document.getElementById('final-score').innerHTML = '';
+  document.getElementById("teams-drawn").innerHTML = "";
+  document.getElementById("final-score").innerHTML = "";
 
   for (let i = 0; i < numPlayers; i++) {
     const position = positions[Math.floor(Math.random() * positions.length)];
-    players.push({ id: i + 1, position, score: 0, inGame: true, bet: initialBet, chips: initialChips - initialBet });
+    players.push({
+      id: i + 1,
+      position,
+      score: 0,
+      inGame: true,
+      bet: initialBet,
+      chips: initialChips - initialBet,
+    });
     pot += initialBet;
   }
 
-  logGameState();  // Log initial game state
+  logGameState(); // Log initial game state
   updatePlayerInfo();
   updatePlayerActions();
 }
 
 function updatePlayerInfo() {
-  const playersSection = document.getElementById('players-section');
+  const playersSection = document.getElementById("players-section");
   playersSection.innerHTML = `<h2>Players' Hands (Pot: ${pot})</h2>`;
-  players.forEach(player => {
-    const status = player.inGame ? 'Active' : 'Folded';
+  players.forEach((player) => {
+    const status = player.inGame ? "Active" : "Folded";
     playersSection.innerHTML += `<div class="player-info"><strong>Player ${player.id} (${status})</strong><ul><li>${player.position}</li><li>Bet: ${player.bet}</li><li>Chips: ${player.chips}</li></ul></div>`;
   });
 }
 
 function updatePlayerActions() {
-  const playerActions = document.getElementById('player-actions');
-  document.getElementById('raiseDiv').style.display = "none";
+  const playerActions = document.getElementById("player-actions");
+  document.getElementById("raiseDiv").style.display = "none";
   if (currentPlayer < players.length && players[currentPlayer].inGame) {
     playerActions.innerHTML = `<h3>Player ${players[currentPlayer].id}'s Turn</h3>`;
     if (players[currentPlayer].bet == currentBet) {
       playerActions.innerHTML += `<button onclick="playerCheck()">Check</button>`;
-    }
-    else {
+    } else {
       playerActions.innerHTML += `<button onclick="playerCall()">Call</button>`;
     }
-    if (currentBet < players[currentPlayer].bet + players[currentPlayer].chips) {//Check if the player can raise
-      playerActions.innerHTML += `<button onclick="toggleRaise()">Raise</button>`
+    if (
+      currentBet <
+      players[currentPlayer].bet + players[currentPlayer].chips
+    ) {
+      //Check if the player can raise
+      playerActions.innerHTML += `<button onclick="toggleRaise()">Raise</button>`;
     }
     playerActions.innerHTML += `
             <button onclick="playerFold()">Fold</button>
@@ -85,13 +128,14 @@ function updatePlayerActions() {
   }
   updatePlayerInfo();
   if (!gameInProgress) {
-    playerActions = document.getElementById('player-actions').innerHTML = '';
+    playerActions = document.getElementById("player-actions").innerHTML = "";
   }
 }
 
 function playerCheck() {
   actions[currentPlayer] = true;
-  if (actions.every(a => a)) { // Check if all players have acted
+  if (actions.every((a) => a)) {
+    // Check if all players have acted
     if (bettingPhase === 3 || drawnTeams.length === 2) {
       gameInProgress = false;
       revealScores();
@@ -116,7 +160,7 @@ function playerCall() {
     pot += diff;
   }
   actions[currentPlayer] = true;
-  if (actions.every(a => a)) {
+  if (actions.every((a) => a)) {
     if (bettingPhase === 3 || drawnTeams.length === 2) {
       gameInProgress = false;
       revealScores();
@@ -135,15 +179,14 @@ function toggleRaise() {
   const raiseDiv = document.getElementById("raiseDiv");
   if (raiseDiv.style.display === "none") {
     raiseDiv.style.display = "initial";
-  }
-  else {
+  } else {
     raiseDiv.style.display = "none";
   }
 }
 
 function playerRaise() {
   const player = players[currentPlayer];
-  betIncrease = Number(document.getElementById('raiseRange').value);
+  betIncrease = Number(document.getElementById("raiseRange").value);
   const raiseAmount = betIncrease;
 
   if (player.chips >= raiseAmount) {
@@ -162,8 +205,8 @@ function playerRaise() {
 }
 
 function updateRaiseAmount() {
-  const raiseAmount = document.getElementById('raiseAmount');
-  const desRaise = document.getElementById('raiseRange').value;
+  const raiseAmount = document.getElementById("raiseAmount");
+  const desRaise = document.getElementById("raiseRange").value;
   raiseAmount.textContent = desRaise;
 }
 
@@ -171,20 +214,24 @@ function updateRaiseBar() {
   const maxRaise = players[currentPlayer].chips;
   document.getElementById("raiseRange").max = maxRaise;
   document.getElementById("maxLabel").innerHTML = maxRaise;
-  document.getElementById("minLabel").innerHTML = currentBet - players[currentPlayer].bet + 1;
-  document.getElementById("raiseRange").min = currentBet - players[currentPlayer].bet + 1;
-  document.getElementById("raiseRange").value = currentBet - players[currentPlayer].bet + 1;
-  document.getElementById("raiseAmount").innerHTML = currentBet - players[currentPlayer].bet + 1;
+  document.getElementById("minLabel").innerHTML =
+    currentBet - players[currentPlayer].bet + 1;
+  document.getElementById("raiseRange").min =
+    currentBet - players[currentPlayer].bet + 1;
+  document.getElementById("raiseRange").value =
+    currentBet - players[currentPlayer].bet + 1;
+  document.getElementById("raiseAmount").innerHTML =
+    currentBet - players[currentPlayer].bet + 1;
 }
 
 function playerFold() {
   players[currentPlayer].inGame = false;
-  activePlayers = activePlayers.filter(index => index !== currentPlayer);
+  activePlayers = activePlayers.filter((index) => index !== currentPlayer);
   actions[currentPlayer] = true;
   if (activePlayers.length === 1) {
     gameInProgress = false;
     revealScores();
-  } else if (actions.every(a => a)) {
+  } else if (actions.every((a) => a)) {
     if (bettingPhase === 3 || drawnTeams.length === 2) {
       gameInProgress = false;
       revealScores();
@@ -216,15 +263,16 @@ function resetPlayer() {
     updatePlayerActions();
   }
 
-  if (!gameInProgress) { //If the game has ended, remove possible actions
-    document.getElementById('player-actions').innerHTML = ''
+  if (!gameInProgress) {
+    //If the game has ended, remove possible actions
+    document.getElementById("player-actions").innerHTML = "";
   }
 }
 
 function drawTeam() {
   const team = teams[Math.floor(Math.random() * teams.length)];
   drawnTeams.push(team);
-  document.getElementById('teams-drawn').innerHTML += `<li>${team}</li>`;
+  document.getElementById("teams-drawn").innerHTML += `<li>${team}</li>`;
   actions.fill(false);
   fillFolded();
   if (drawnTeams.length < 2) {
@@ -247,29 +295,38 @@ function fillFolded() {
 }
 
 function revealScores() {
-  document.getElementById('player-actions').innerHTML = '';
+  document.getElementById("player-actions").innerHTML = "";
   gameInProgress = false;
-  players.forEach(player => {
-    drawnTeams.forEach(team => {
+  players.forEach((player) => {
+    drawnTeams.forEach((team) => {
       if (teamScores[team][player.position] && player.inGame) {
         player.score += teamScores[team][player.position];
       }
     });
   });
 
-  const remainingPlayers = players.filter(p => p.inGame);
-  const winner = remainingPlayers.reduce((prev, current) => (prev.score > current.score ? prev : current), { score: -1 });
+  const remainingPlayers = players.filter((p) => p.inGame);
+  const winner = remainingPlayers.reduce(
+    (prev, current) => (prev.score > current.score ? prev : current),
+    { score: -1 }
+  );
   winner.chips += pot;
 
   revealWinner(winner);
 }
 
 function revealWinner(winner) {
-  const scoresText = players.map(p => {
-    const activePlayers = drawnTeams.map(team => activePlayersData[team][p.position]).join(', ');
-    return `Player ${p.id}: ${p.score} points, Chips: ${p.chips}, Active Football Players: ${activePlayers}`;
-  }).join('<br>');
-  document.getElementById('final-score').innerHTML = `Scores:<br>${scoresText}<br><br>Winner: Player ${winner.id} with ${winner.score} points! Pot: ${pot}`;
+  const scoresText = players
+    .map((p) => {
+      const activePlayers = drawnTeams
+        .map((team) => activePlayersData[team][p.position])
+        .join(", ");
+      return `Player ${p.id}: ${p.score} points, Chips: ${p.chips}, Active Football Players: ${activePlayers}`;
+    })
+    .join("<br>");
+  document.getElementById(
+    "final-score"
+  ).innerHTML = `Scores:<br>${scoresText}<br><br>Winner: Player ${winner.id} with ${winner.score} points! Pot: ${pot}`;
 }
 
 function logGameState() {
@@ -277,8 +334,12 @@ function logGameState() {
   console.log(`Current Bet: ${currentBet}`);
   console.log(`Pot: ${pot}`);
   console.log(`Betting Phase: ${bettingPhase}`);
-  console.log(`Drawn Teams: ${drawnTeams.join(', ')}`);
-  console.log(`Player Actions: ${actions.map((action, index) => `Player ${index + 1}: ${action}`).join(', ')}`);
+  console.log(`Drawn Teams: ${drawnTeams.join(", ")}`);
+  console.log(
+    `Player Actions: ${actions
+      .map((action, index) => `Player ${index + 1}: ${action}`)
+      .join(", ")}`
+  );
 }
 
 startGame();
